c394826e0ed50b43ebf5db0d67aa5b9b1c6342b0
peynadol <peynadol@gmail.com>
2025-06-25 17:44:56 +0100

    add route to CTA contact us button

--- src/lib/components/CTA.svelte
+++ src/lib/components/CTA.svelte
@@ -13,6 +13,8 @@
 		class="relative z-10 mx-auto flex max-w-xl flex-col items-center gap-6 text-center md:flex-row md:justify-between"
 	>
 		<h2 class="text-sacramento-green text-[32px] font-bold">Ready to get started?</h2>
-		<Button variant="primary" theme="dark">contact us</Button>
+		<a href="/contact">
+			<Button variant="primary" theme="dark">contact us</Button>
+		</a>
 	</div>
 </section>
